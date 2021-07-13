9012b97ea23086a7c82a541fe1dc7f9fcee4a2ac
Jorge Cabiedes <57368278+PrimeBIue@users.noreply.github.com>
2021-07-13 13:40:05 -0500

    All fragment templates are ready

--- app/src/main/java/com/example/icebreaker/MainActivity.java
+++ app/src/main/java/com/example/icebreaker/MainActivity.java
@@ -3,11 +3,15 @@ package com.example.icebreaker;
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
 
 import android.os.Bundle;
 import android.view.MenuItem;
 import android.widget.Toast;
 
+import com.example.icebreaker.Fragments.HobbiesFragment;
+import com.example.icebreaker.Fragments.IcebreakFragment;
+import com.example.icebreaker.Fragments.ProfileFragment;
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 public class MainActivity extends AppCompatActivity {
@@ -19,6 +23,8 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        final FragmentManager fragmentManager = getSupportFragmentManager();
+
         final BottomNavigationView bottomNavigationView;
 
         bottomNavigationView = findViewById(R.id.bottomNavigation);
@@ -28,20 +34,27 @@ public class MainActivity extends AppCompatActivity {
             public boolean onNavigationItemSelected(@NonNull MenuItem item) {
                 Fragment fragment = null;
                 switch (item.getItemId()) {
-                    case R.id.action_home:
-                        Toast.makeText(MainActivity.this, "Home!", Toast.LENGTH_SHORT).show();
+                    case R.id.action_hobbies:
+                        Toast.makeText(MainActivity.this, "Hobbies!", Toast.LENGTH_SHORT).show();
+                        fragment = new HobbiesFragment();
                         break;
-                    case R.id.action_compose:
+                    case R.id.action_icebreak:
                         Toast.makeText(MainActivity.this, "IceBreak!", Toast.LENGTH_SHORT).show();
+                        fragment = new IcebreakFragment();
                         break;
                     case R.id.action_profile:
                         Toast.makeText(MainActivity.this, "Profile!", Toast.LENGTH_SHORT).show();
+                        fragment = new ProfileFragment();
                         break;
                     default:
                         break;
                 }
+                fragmentManager.beginTransaction().replace(R.id.flContainer, fragment).commit();
                 return true;
             }
         });
+
+        // Default selection
+        bottomNavigationView.setSelectedItemId(R.id.action_hobbies);
     }
 }
