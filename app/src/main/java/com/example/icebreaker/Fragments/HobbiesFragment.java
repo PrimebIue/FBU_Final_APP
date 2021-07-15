ae1927aaade6cdededf22867f9212d224e061077
Jorge Cabiedes <57368278+PrimeBIue@users.noreply.github.com>
2021-07-15 10:12:39 -0500

    Set the adapter on the hobbies fragment

--- app/src/main/java/com/example/icebreaker/Fragments/HobbiesFragment.java
+++ app/src/main/java/com/example/icebreaker/Fragments/HobbiesFragment.java
@@ -5,17 +5,30 @@ import android.os.Bundle;
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import com.example.icebreaker.HobbiesAdapter;
 import com.example.icebreaker.R;
+import com.example.icebreaker.Subclasses.Hobby;
+import com.parse.ParseQuery;
+
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * A simple {@link Fragment} subclass.
  */
 public class HobbiesFragment extends Fragment {
+    
+    public static final String TAG = "HobbiesFragment";
+    
+    private HobbiesAdapter adapter;
+    private List<Hobby> allHobbies;
 
     public HobbiesFragment() {
         // Required empty public constructor
@@ -31,5 +44,26 @@ public class HobbiesFragment extends Fragment {
     @Override
     public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
+        
+        final RecyclerView rvHobbies = view.findViewById(R.id.rvHobbies);
+        
+        // Initialize the array
+        allHobbies = new ArrayList<>();
+        adapter = new HobbiesAdapter(getContext(), allHobbies);
+        
+        // Set the adapter
+        rvHobbies.setAdapter(adapter);
+        
+        // Create layout manager
+        LinearLayoutManager linearLayoutManager = new LinearLayoutManager(getContext());
+        rvHobbies.setLayoutManager(linearLayoutManager);
+        
+        queryHobbies();
+    }
+
+    private void queryHobbies() {
+        // Specify data to query
+        ParseQuery<Hobby> query =  ParseQuery.getQuery(Hobby.class);
+        // TODO--Create proper relation on Back4App to query correctly
     }
 }
