396ab45ed702cc0c661712fbb71aa9f5de0e7440
Jorge Cabiedes <57368278+PrimeBIue@users.noreply.github.com>
2021-07-21 10:35:13 -0500

    Prepared UserPairingActivity and queries for hobby pairing

--- app/src/main/java/com/fbu/icebreaker/UserPairingActivity.java
+++ app/src/main/java/com/fbu/icebreaker/UserPairingActivity.java
@@ -4,22 +4,31 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.media.Image;
 import android.os.Bundle;
+import android.util.Log;
 import android.widget.ImageView;
 import android.widget.TextView;
 
+import com.fbu.icebreaker.subclasses.Hobby;
+import com.parse.FindCallback;
 import com.parse.ParseException;
 import com.parse.ParseQuery;
 import com.parse.ParseUser;
 
 import org.w3c.dom.Text;
 
+import java.util.List;
+
 public class UserPairingActivity extends AppCompatActivity {
+
+    private static final String TAG = "UserPairingActivity";
 
     private ImageView tvProfilePicture;
     private TextView tvUsername;
     private TextView tvHobbiesNumber;
     private TextView tvBio;
     private ParseUser user;
+    private List<Hobby> currUserHobbies;
+    private List<Hobby> qrHobbies;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -39,5 +48,51 @@ public class UserPairingActivity extends AppCompatActivity {
         }
 
         tvUsername.setText(user.getUsername());
+    }
+
+    private void queryCurrUserHobbies() {
+
+        // Specify data to query
+        ParseQuery<Hobby> query =  ParseQuery.getQuery(Hobby.class);
+        query.include("usersWithHobby");
+        query.whereEqualTo("usersWithHobby", ParseUser.getCurrentUser());
+        query.findInBackground(new FindCallback<Hobby>() {
+            @Override
+            public void done(List<Hobby> hobbies, ParseException e) {
+                // Check for errors
+                if (e != null) {
+                    Log.e(TAG, "Issue with getting hobbies.", e);
+                }
+
+                for (Hobby hobby : hobbies) {
+                    Log.i(TAG, "Hobby: " + hobby.getName());
+                }
+                Log.i(TAG, "Gets here" + hobbies);
+                currUserHobbies = hobbies;
+            }
+        });
+    }
+
+    private void queryQRHobbies() {
+
+        // Specify data to query
+        ParseQuery<Hobby> query =  ParseQuery.getQuery(Hobby.class);
+        query.include("usersWithHobby");
+        query.whereEqualTo("usersWithHobby", ParseUser.getCurrentUser());
+        query.findInBackground(new FindCallback<Hobby>() {
+            @Override
+            public void done(List<Hobby> hobbies, ParseException e) {
+                // Check for errors
+                if (e != null) {
+                    Log.e(TAG, "Issue with getting hobbies.", e);
+                }
+
+                for (Hobby hobby : hobbies) {
+                    Log.i(TAG, "Hobby: " + hobby.getName());
+                }
+                Log.i(TAG, "Gets here" + hobbies);
+                qrHobbies = hobbies;
+            }
+        });
     }
 }
