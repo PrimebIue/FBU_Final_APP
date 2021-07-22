f8dc33581d9e84dfc2b02b98bf8373c323106d9d
Jorge Cabiedes <57368278+PrimeBIue@users.noreply.github.com>
2021-07-22 14:50:27 -0500

    Multiple existing hobby selection ready

--- app/src/main/java/com/fbu/icebreaker/MultiSelectionAdapter.java
+++ app/src/main/java/com/fbu/icebreaker/MultiSelectionAdapter.java
@@ -1,10 +1,12 @@
 package com.fbu.icebreaker;
 
 import android.content.Context;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.RelativeLayout;
+import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.core.content.ContextCompat;
@@ -12,22 +14,21 @@ import androidx.recyclerview.widget.RecyclerView;
 
 import com.fbu.icebreaker.subclasses.Hobby;
 
+import org.w3c.dom.Text;
+
 import java.util.ArrayList;
+import java.util.List;
 
 public class MultiSelectionAdapter extends RecyclerView.Adapter<MultiSelectionAdapter.MultiViewHolder> {
 
-    private final Context context;
-    private ArrayList<Hobby> hobbies;
+    private static final String TAG = "MultiSelectionAdapter";
 
-    public MultiSelectionAdapter(Context context, ArrayList<Hobby> hobbies) {
+    private final Context context;
+    private List<Hobby> hobbies;
+
+    public MultiSelectionAdapter(Context context, List<Hobby> hobbies) {
         this.context = context;
         this.hobbies = hobbies;
-    }
-
-    public void setHobbies(ArrayList<Hobby> hobbies) {
-        this.hobbies = new ArrayList<>();
-        this.hobbies = hobbies;
-        notifyDataSetChanged();
     }
 
     @NonNull
@@ -39,43 +40,49 @@ public class MultiSelectionAdapter extends RecyclerView.Adapter<MultiSelectionAd
 
     @Override
     public void onBindViewHolder(@NonNull MultiViewHolder holder, int position) {
-        holder.bind(hobbies.get(position));
+        Hobby hobby = hobbies.get(position);
+        holder.bind(hobby);
     }
 
     @Override
-    public int getItemCount() {
-        return 0;
-    }
+    public int getItemCount() { return hobbies.size(); }
 
     class MultiViewHolder extends RecyclerView.ViewHolder {
 
         private final RelativeLayout rlHobbySelection;
+        private TextView tvHobbyName;
+        private TextView tvEmoji;
 
         public MultiViewHolder(@NonNull View itemView) {
             super(itemView);
             rlHobbySelection = itemView.findViewById(R.id.rlHobbySelection);
+            tvHobbyName = itemView.findViewById(R.id.tvHobbyName);
+            tvEmoji = itemView.findViewById(R.id.tvEmoji);
         }
 
         void bind(final Hobby hobby) {
-            rlHobbySelection.setBackgroundColor(hobby.getChecked() ? ContextCompat.getColor(context, R.color.gray_500) : ContextCompat.getColor(context, R.color.light_blue));
+            rlHobbySelection.setBackgroundColor(hobby.getChecked() ? ContextCompat.getColor(context, R.color.light_blue) : ContextCompat.getColor(context, R.color.gray_500));
+            tvHobbyName.setText(hobby.getName());
+            tvEmoji.setText(hobby.getEmoji());
 
             itemView.setOnClickListener(new View.OnClickListener() {
                 @Override
                 public void onClick(View v) {
                     hobby.setChecked(!hobby.getChecked());
-                    rlHobbySelection.setBackgroundColor(hobby.getChecked() ? ContextCompat.getColor(context, R.color.gray_500) : ContextCompat.getColor(context, R.color.light_blue));
+                    rlHobbySelection.setBackgroundColor(hobby.getChecked() ? ContextCompat.getColor(context, R.color.light_blue) : ContextCompat.getColor(context, R.color.gray_500));
+                    Log.i(TAG, String.valueOf(hobby.getChecked()));
                 }
             });
         }
     }
 
     // Get all items selected
-    public ArrayList<Hobby> getAll() { return hobbies; }
+    public List<Hobby> getAll() { return hobbies; }
 
     // Get selected when button is clicked
 
-    public ArrayList<Hobby> getSelected() {
-        ArrayList<Hobby> selected = new ArrayList<>();
+    public List<Hobby> getSelected() {
+        List<Hobby> selected = new ArrayList<>();
         for (int i = 0; i < hobbies.size(); i++){
             if (hobbies.get(i).getChecked()) {
                 selected.add(hobbies.get(i));
--- app/src/main/java/com/fbu/icebreaker/fragments/AddHobbyFragment.java
+++ app/src/main/java/com/fbu/icebreaker/fragments/AddHobbyFragment.java
@@ -6,6 +6,8 @@ import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.DialogFragment;
 import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.util.Log;
 import android.view.LayoutInflater;
@@ -15,11 +17,19 @@ import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
 
+import com.fbu.icebreaker.MultiSelectionAdapter;
 import com.fbu.icebreaker.R;
 import com.fbu.icebreaker.subclasses.Hobby;
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+import com.parse.FindCallback;
 import com.parse.ParseException;
+import com.parse.ParseQuery;
 import com.parse.ParseUser;
 import com.parse.SaveCallback;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.List;
 
 /**
  * A simple {@link Fragment} subclass.
@@ -28,8 +38,10 @@ public class AddHobbyFragment extends DialogFragment {
 
     private static final String TAG = "AddHobbyFragment";
 
-    private EditText etHobbyName;
-    private EditText etEmoji;
+    private RecyclerView rvHobbySelector;
+    private List<Hobby> allHobbies;
+    private MultiSelectionAdapter adapter;
+    private FloatingActionButton btnAdd;
 
     public AddHobbyFragment() {
         // Required empty public constructor
@@ -46,35 +58,64 @@ public class AddHobbyFragment extends DialogFragment {
     public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
         // Get field from view
-        etHobbyName = (EditText) view.findViewById(R.id.etHobbyName);
-        etEmoji = (EditText) view.findViewById(R.id.etEmoji);
-        final Button btnAdd = view.findViewById(R.id.btnAdd);
+        rvHobbySelector = view.findViewById(R.id.rvHobbySelector);
+        btnAdd = view.findViewById(R.id.btnAdd);
 
-        /*ParseObject hobby = new Hobby();*/
+
+        LinearLayoutManager layoutManager = new LinearLayoutManager(getContext());
+        rvHobbySelector.setLayoutManager(layoutManager);
+
+        allHobbies = new ArrayList<>();
+        adapter = new MultiSelectionAdapter(getContext(), allHobbies);
+        rvHobbySelector.setAdapter(adapter);
+
+        queryHobbies();
+
         btnAdd.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                // Create Parse hobby object
-                Hobby hobby = new Hobby();
+                if (adapter.getSelected().size() > 0) {
 
-                String hobbyName = etHobbyName.getText().toString();
-                String emoji = etEmoji.getText().toString();
-                hobby.setEmoji(emoji);
-                hobby.setName(hobbyName);
-                hobby.getRelation("usersWithHobby").add(ParseUser.getCurrentUser());
-                hobby.saveInBackground(new SaveCallback() {
-                    @Override
-                    public void done(ParseException e) {
-                        if (e != null) {
-                            Log.e(TAG, "Error while saving hobby.", e);
-                            Toast.makeText(getContext(), R.string.saving_error, Toast.LENGTH_SHORT).show();
-                        }
-                        Log.i(TAG, "Hobby save successful");
-                        etHobbyName.setText("");
-                        etEmoji.setText("");
-                        getDialog().dismiss();
+                    // TODO -- Only update hobby relation not create new hobby
+                    for (int i = 0; i < adapter.getSelected().size(); i++) {
+
+                        adapter.getSelected().get(i).getRelation("usersWithHobby").add(ParseUser.getCurrentUser());
+                        adapter.getSelected().get(i).saveInBackground(new SaveCallback() {
+                            @Override
+                            public void done(ParseException e) {
+                                if (e != null) {
+                                    Log.e(TAG, "Error while saving hobby.", e);
+                                    Toast.makeText(getContext(), R.string.saving_error, Toast.LENGTH_SHORT).show();
+                                }
+                                Log.i(TAG, "Hobby save successful");
+                            }
+                        });
                     }
-                });
+                }
+                getDialog().dismiss();
+            }
+        });
+    }
+
+    private void queryHobbies() {
+
+        // TODO -- This currently gets all hobbies, it eventually has to only include hobbies the user doesnt already have
+        // Specify data to query
+        ParseQuery<Hobby> query =  ParseQuery.getQuery(Hobby.class);
+        query.findInBackground(new FindCallback<Hobby>() {
+            @Override
+            public void done(List<Hobby> hobbies, ParseException e) {
+                // Check for errors
+                if (e != null) {
+                    Log.e(TAG, "Issue with getting hobbies.", e);
+                }
+
+                for (Hobby hobby : hobbies) {
+                    Log.i(TAG, "Hobby: " + hobby.getName());
+                }
+                Log.i(TAG, "Gets here" + hobbies);
+                allHobbies.addAll(hobbies);
+                adapter.notifyDataSetChanged();
             }
         });
     }
