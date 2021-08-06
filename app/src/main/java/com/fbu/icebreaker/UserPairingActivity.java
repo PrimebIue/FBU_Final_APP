9d41dec60a25f9e64e0578c7500c636bcd6c6cbe
Jorge Cabiedes <57368278+PrimeBIue@users.noreply.github.com>
2021-08-06 12:58:35 -0500

    Added confetti animation when pairing user

--- app/src/main/java/com/fbu/icebreaker/UserPairingActivity.java
+++ app/src/main/java/com/fbu/icebreaker/UserPairingActivity.java
@@ -1,12 +1,20 @@
 package com.fbu.icebreaker;
 
+import android.graphics.Color;
+import android.graphics.drawable.Drawable;
 import android.os.Bundle;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
+import androidx.core.content.ContextCompat;
 import androidx.viewpager.widget.ViewPager;
 
 import com.fbu.icebreaker.adapters.ViewPagerAdapter;
+
+import nl.dionsegijn.konfetti.KonfettiView;
+
+import nl.dionsegijn.konfetti.models.Shape;
+import nl.dionsegijn.konfetti.models.Size;
 
 public class UserPairingActivity extends AppCompatActivity {
 
@@ -20,6 +28,19 @@ public class UserPairingActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_user_pairing);
+
+        final KonfettiView konfettiView = findViewById(R.id.viewKonfetti);
+
+        konfettiView.build()
+                .addColors(Color.CYAN, Color.YELLOW, Color.WHITE)
+                .setDirection(0.0, 359.0)
+                .setSpeed(1f, 5f)
+                .setFadeOutEnabled(true)
+                .setTimeToLive(2000L)
+                .addShapes(Shape.Square.INSTANCE, Shape.Circle.INSTANCE)
+                .addSizes(new Size(12, 5f))
+                .setPosition(-50f, konfettiView.getWidth() + 2000f, -50f, -50f)
+                .streamFor(300, 2500L);
 
         toolbar = findViewById(R.id.tbTab);
         viewPager = findViewById(R.id.pager);
